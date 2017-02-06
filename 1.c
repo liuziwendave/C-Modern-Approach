bb3bb6ca2ac4475cd2f77d86856769b7e2e1324c
liuziwendave <Dave@LIUs-MacBook-Pro.local>
2017-02-06 22:29:19 +1100

    Exercise in Chapter 1

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char **argv){
+
+   printf("         *\n");
+   printf("        *\n");
+   printf("       * \n");
+   printf("  *   *\n");
+   printf("   * *\n");
+   printf("    *\n");
+
+
+   return EXIT_SUCCESS;
+}  
